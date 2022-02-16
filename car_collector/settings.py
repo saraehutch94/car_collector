236a3a56f3c08a321d6a8db11c5b4b7abd9a2fd1
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-16 11:38:52 -0500

    initialize main_app / install app to core project / applied migrations

--- car_collector/settings.py
+++ car_collector/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+ 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -75,8 +76,8 @@ WSGI_APPLICATION = 'car_collector.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'car_collector',
     }
 }
 
