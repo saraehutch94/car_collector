6a9e20afaeb932613f93edf0b25c0bb3b3e2ecd1
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-16 13:53:59 -0500

    add cars_index views function to views.py / rendering car data

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
@@ -8,3 +7,6 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def cars_index(request):
+    return render(request, 'cars/index.html', {'cars': cars})
