76560a431d751bcb1064ef8cf8869413da227b87
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-16 14:00:07 -0500

    add fake database and list of cars for test of rendering data

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,23 @@
 from django.shortcuts import render
 
+'''
+This is our fake database (changing later):
+'''
+
+class Car:
+    def __init__(self, make, model, color, description):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.description = description
+
+# list of cars
+cars = [
+    Car('Tesla', 'Model X', 'blue', 'my future car that will be named blueberry'),
+    Car('Kia', 'EV6', 'silver', 'wowee'),
+    Car('Kia', 'Stinger', 'Ascot Green', 'fast car'),
+]
+
 # Create your views here.
 
 def home(request):
