0ac7c3214b0ca9ceeaf9f12a7127707cb371db54
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-16 21:11:25 -0500

    add Car model / string dunder method

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    color = models.CharField(max_length=30)
+    description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return self.make
