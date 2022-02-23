b8b08129127ffd27a545e177043520a37dc26d82
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-23 15:39:51 -0500

    import necessary class views

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,7 @@
 from re import L
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Car
 
