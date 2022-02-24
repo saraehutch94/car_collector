a8ba06a06ef05456a625778ca70539cf85afa0bb
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-24 12:28:47 -0500

    register Tree and Gas models to admin portal

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Car
+from .models import Car, Tree, Gas
 
 # Register your models here.
 admin.site.register(Car)
+admin.site.register(Tree)
+admin.site.register(Gas)
