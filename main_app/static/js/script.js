6d94d4f32691ee68479a157bcd4e6942b2a6d344
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-02-24 14:32:52 -0500

    configure date picker class

--- main_app/static/js/script.js
+++ main_app/static/js/script.js
@@ -1,1 +1,10 @@
-console.log(M);
+// Date Picker Animation
+
+const dateEl = document.getElementById("id_date");
+
+M.Datepicker.init(dateEl, {
+  format: "yyyy-mm-dd",
+  defaultDate: new Date(),
+  setDefaultDate: true,
+  autoClose: true,
+});
