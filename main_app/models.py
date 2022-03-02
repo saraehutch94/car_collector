877a8afdfe59e61653cb432f46eab2155bf7557c
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-02 12:58:01 -0500

    remove user attribute on Car model / attempt to fix merging issue

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -28,7 +27,6 @@ class Car(models.Model):
     color = models.CharField(max_length=30)
     description = models.TextField(max_length=250)
     trees = models.ManyToManyField(Tree)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return (f'{self.make} {self.model}')
