3b5549746377801bd5f6966384f36ede42da3f9d
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-03-03 14:02:49 -0500

    add Photo model to admin site

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Car, Tree, Gas
+from .models import Car, Tree, Gas, Photo
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(Tree)
 admin.site.register(Gas)
+admin.site.register(Photo)
